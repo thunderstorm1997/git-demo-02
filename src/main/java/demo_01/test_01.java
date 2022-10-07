9c1b1848e960395ea55c7cf35811dfb08a0e1939
Rick <Rick@123.com>
2022-10-07 14:54:36 +0800

    master commited 02

--- src/main/java/demo_01/test_01.java
+++ src/main/java/demo_01/test_01.java
@@ -4,5 +4,6 @@ public class test_01 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hot fix");
+        System.out.println("master test");
     }
 }
